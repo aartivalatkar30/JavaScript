2ce69d898740a54864f43808dc09b7bab84fd2f7
aartivalatkar30 <2020.aarti.valatkar@ves.ac.in>
2024-04-18 22:03:37 +0530

    javascript

---
+++ Day1/index.js
@@ -0,0 +1,7 @@
+var mycollege="VESIT";
+var collegeId=10001
+var mycollegeAssignment=true;
+
+console.log(mycollege );
+console.log(collegeId);
+console.log(mycollegeAssignment);
---
+++ Day2/index.js
@@ -0,0 +1,60 @@
+console.log("************TypeOF Opeartors************")
+var mathsscore=90;
+var englishscore=92;
+
+console.log("ADD score:"+mathsscore+englishscore);
+console.log("Sub score:"+mathsscore+englishscore);
+console.log("Mul score:"+mathsscore*englishscore);
+console.log("division:"+mathsscore/englishscore);
+
+console.log("**********Typeof Datatypes**************")
+
+console.log(1, 2, typeof(1))
+console.log("hello is a typeof ", typeof(" hello"))
+console.log('hello is a typeof ', typeof('hello'))
+console.log(`hello is a typeof `, typeof(` hello`))
+console.log(true, typeof(true))
+console.log(false, typeof(false))
+
+
+console.log("**********Typeof Datatypes**************")
+
+console.log(typeof("Institute"))
+console.log(typeof(true))
+console.log(typeof(23))
+console.log(typeof(`true`))
+console.log(typeof('1111'))
+console.log("*******************************")
+
+var myname = "AARTI VALATKAR";
+console.log(myname ,typeof(myname));
+
+
+console.log("***********Typeof Undefined********************")
+
+// undefined
+var todaysStudent
+console.log(todaysStudent , typeof(todaysStudent))
+
+
+
+console.log(myClass)
+var myClass = 20;
+console.log("its type of  declared : "+myClass)
+myClass = 30; 
+console.log("its type of resassigning : "+myClass)
+var myClass = 40;
+console.log("type of redeclared : "+myClass)
+
+
+console.log("******VAR********LET***********CONST******")
+
+let print = "my name is aarti ";
+print = "i am aarti";
+console.log(print)
+
+const number = 77777;
+// const number = 5555;
+// number = 44444;
+console.log(number);
+
