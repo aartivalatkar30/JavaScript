ca5d06f153034a151906b17108b670dadb0fd38b
aartivalatkar30 <2020.aarti.valatkar@ves.ac.in>
2024-04-18 22:21:29 +0530

    var , let ,hoist
    var , let ,hoist

---
+++ Day3/index.js
@@ -0,0 +1,51 @@
+function Greet(){
+    console.log("Thank You!!!!");
+
+}
+
+console.log("Function::"+Greet());
+
+{
+    var number = 100;
+  }
+  console.log(number);
+
+  {
+    let number = 80;
+    {
+      console.log(number);
+    }
+  }
+  console.log("---------let-------------");
+
+  let firstno=50;
+  {
+
+    const secondno =25;
+    var thirdno=100;
+    console.log(
+        firstno);
+    {
+    {
+        console.log("secondno: "+secondno);
+        console.log("thirdno : "+thirdno);
+        console.log("firstno : "+firstno);
+
+    }
+    console.log("secondno"+secondno);
+  }
+}
+console.log(thirdno)
+
+console.log("---------hoisting-------")
+
+numeric = 10;
+console.log(""+numeric);
+
+var numeric;
+
+
+numeric = 10;
+console.log(numeric);
+
+let numeric;
