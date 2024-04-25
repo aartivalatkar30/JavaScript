bb009c7d818654690214bfeeddb87482df83c428
aartivalatkar30 <2020.aarti.valatkar@ves.ac.in>
2024-04-25 18:38:59 +0530

    even-odd
    onclick

---
+++ Day5/index.js
@@ -0,0 +1,19 @@
+function CheckNumber() {
+    //   console.log(document.getElementById("number").value);
+    //   alert("Function called.");
+    const number = document.getElementById("number").value;
+    console.log(number, "number");
+    if (!number) {
+      alert("Please type the number.");
+    } else if (number % 2 === 0) {
+      alert(`Number ${number} is Even.`);
+    } else {
+      alert(`Number ${number} is Odd.`);
+    }
+
+   
+
+  }
+
+
+ 
